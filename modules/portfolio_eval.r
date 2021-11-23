0ed42dec5c74dcb74d9b735756e0c29ca4c9bc1e
dlhjel <dlhjel@gmail.com>
2021-11-23 12:37:14 -0500

    changed risk/reward plot to color holdings

--- modules/portfolio_eval.r
+++ modules/portfolio_eval.r
@@ -8,7 +8,8 @@ portfolio_eval_test <- function(twri=NULL, twrib=NULL) {
                      twrib = twrib,
                      from = '2015-12-31',
                      to   = '2021-11-30',
-                     plottype = 'cria')
+                     plottype = 'cria',
+                     label = 'symbol')
 }
 
 portfolio_eval <- function(holding,
@@ -18,13 +19,17 @@ portfolio_eval <- function(holding,
                            from,
                            to  ,
                            period='months',
-                           plottype='icra') {
+                           plottype='icra',
+                           label = 'symbol',
+                           portfolioname=NULL) {
     ## given: holding   = vector of 1 or more symbols of holdings in portfolio
     ##        weight   = vector of weights for each holding (needs to sum to 1)
     ##        from     = start date
     ##        to       = end date
     ##        twri     = xts object with output from equitytwr, if provided, for holdings
     ##        twrib    = xts object with output from equitytwr for bench
+    ##        label    = 'symbol' uses holding symbols on risk/return and beta/alpha plots
+    ##                 = 'simple' collapses all holdings to "holding"
     ## create plots of: risk/reward for portfolio, holdings, and benchmark
     ##                  performance history by year (bar chart?)
     ##                  alpha/beta for portfolio, holdings, and benchmark
@@ -83,27 +88,53 @@ portfolio_eval <- function(holding,
     
     ##-----------------------------------------------------------------------------
     ## plot cumulative TWR
-    if (grepl('c', plottype)) plot( plotxts(twrcum ) )
+    if (grepl('c', plottype)) plot( plotxts(twrcum,
+                                            main=portfolioname) )
 
 
     ##-----------------------------------------------------------------------------
-    ## risk/reward plot
-
     ## create dataframe of holdings, 'portfolio', and 'benchmark'
     out       <- data.frame(colnames(twriall))
-    colnames(out) <- 'key'
-    out$label <- 'holding'
-    out[out$key == 'portfolio',]$label <- 'portfolio'
-    out[out$key == 'benchmark',]$label <- 'benchmark'
+    colnames(out) <- 'Holding'
+    if (label == 'symbol') {
+        ## use holding symbols on risk/return and beta/alpha plots
+        out$lable <- out$Holding
+    } else {
+        ## use labels of 'holding', 'portfolio', or 'benchmark' on plot
+        out$label <- 'holding'
+        out[out$Holding == 'portfolio',]$label <- 'portfolio'
+        out[out$Holding == 'benchmark',]$label <- 'benchmark'
+    }
     rr  <- cbind(out, twrcuml, std)
-    names(rr) <- c('key', 'label', 'twrcum', 'std')
+    names(rr) <- c('Holding', 'label', 'twrcum', 'std')
+
+
+    ##-----------------------------------------------------------------------------
+    ## add alpha and beta to rr
+    rr$alpha <- NA
+    rr$beta  <- NA
+    benchi <- as.numeric(twriall[, ncol(twriall)])
+    for (i in 1:ncol(twriall)) {
+        out <- alpha_beta(as.numeric(twriall[,i]), benchi, plot=FALSE)
+        rr[i,]$alpha <- out$alpha
+        rr[i,]$beta  <- out$beta
+    }
+
+    ## separate into 3 dataframes
+    rrhold  <- rr[rr$Holding != 'portfolio' & rr$Holding != 'benchmark',]
+    rrport  <- rr[rr$Holding == 'portfolio',]
+    rrbench <- rr[rr$Holding == 'benchmark',]
+
     
+    ##-----------------------------------------------------------------------------
+    ## risk/reward plot
+
     if (grepl('r', plottype)) {
 
         ## separate into 3 dataframes
-        rrhold <- rr[rr$key != 'portfolio' & rr$key != 'benchmark',]
-        rrport <- rr[rr$key == 'portfolio',]
-        rrbench <- rr[rr$key == 'benchmark',]
+        rrhold  <- rr[rr$Holding != 'portfolio' & rr$Holding != 'benchmark',]
+        rrport  <- rr[rr$Holding == 'portfolio',]
+        rrbench <- rr[rr$Holding == 'benchmark',]
         
         ## determine reange making room for legend
         xrange <- range(rr$std, efdata$ef$efstd)
@@ -111,89 +142,85 @@ portfolio_eval <- function(holding,
                     max(xrange) + 0.25*(max(xrange) - min(xrange)))
         ylim   <- range(rr$twrcum, efdata$ef$eftwrcum)
 
-        ## ## simple option with color but benchmark and portfolio scattered throughout
-        ## with(rr, plotfit(std, twrcum, label, interval='noline',
-        ##                      xlimspec=xlim, ylimspec=ylim,
-        ##                      xlabel = 'Standard Deviation',
-        ##                      ylabel = 'Cumulative TWR'))
-        ## mtext('(solid line = Schwab EF; dotted line = S&P 500 / AGG)',
-        ##       side=3, line=1, cex=1)
-        ## ## add efficient frontier line
-        ## out <- ef(model='Schwab', efdata=efdata, addline=TRUE, col='black', lty=1, pch=3)
-        ## out <- ef(model='simple', efdata=efdata, addline=TRUE, col='black', lty=2, pch=4)
-        
-        ## add holdings
-        plot(rrhold$std, rrhold$twrcum,
-             xlab = 'Standard Deviation',
-             ylab = 'Cumulative TWR',
-             col  = 'black', pch=1,
-             xlim=xlim, ylim=ylim)
-        grid(col='gray70')
+        ## plot holdings
+        with(rrhold, plotfit(std, twrcum, label, interval='noline',
+                             xlimspec=xlim, ylimspec=ylim,
+                             xlabel = 'Standard Deviation',
+                             ylabel = 'Cumulative TWR'))
         ## add portfolio
         points(rrport$std, rrport$twrcum, col='red', pch=16)
         ## add benchmark
-        points(rrbench$std, rrbench$twrcum, col='magenta1', pch=17)
-
+        points(rrbench$std, rrbench$twrcum, col='blue', pch=17)
         ## add efficient frontier line
         out <- ef(model='Schwab', efdata=efdata, addline=TRUE, col='black', lty=1, pch=3)
         out <- ef(model='simple', efdata=efdata, addline=TRUE, col='black', lty=2, pch=4)
+        mtext('(portfolio = solid red circle; benchmark = solid blue triangle)',
+              side=3, line=1, cex=1)
+        mtext('(Schwab EF = solid line; S&P 500 / AGG EF = dotted line)',
+              side=3, line=0, cex=1)
 
-        ## add legend
-        legend('bottomright', title=NULL,
-               col    = c('black',   'red',       'magenta1',  'black',     'black'),
-               legend = c('holding', 'portfolio', 'benchmark', 'Schwab EF', 'S&P 500 / AGG'),
-               lty    = c(NA,        NA,          NA,           1,           2),
-               pch    = c(1,         16,          17,           3,           4))
+            
+        ## ## add holdings
+        ## plot(rrhold$std, rrhold$twrcum,
+        ##      xlab = 'Standard Deviation',
+        ##      ylab = 'Cumulative TWR',
+        ##      col  = 'black', pch=1,
+        ##      xlim=xlim, ylim=ylim)
+        ## grid(col='gray70')
+        ## ## add portfolio
+        ## points(rrport$std, rrport$twrcum, col='red', pch=16)
+        ## ## add benchmark
+        ## points(rrbench$std, rrbench$twrcum, col='magenta1', pch=17)
+        ## 
+        ## ## add efficient frontier line
+        ## out <- ef(model='Schwab', efdata=efdata, addline=TRUE, col='black', lty=1, pch=3)
+        ## out <- ef(model='simple', efdata=efdata, addline=TRUE, col='black', lty=2, pch=4)
+        ## 
+        ## ## add legend
+        ## legend('bottomright', title=NULL,
+        ##        col    = c('black',   'red',       'magenta1',  'black',     'black'),
+        ##        legend = c('holding', 'portfolio', 'benchmark', 'Schwab EF', 'S&P 500 / AGG'),
+        ##        lty    = c(NA,        NA,          NA,           1,           2),
+        ##        pch    = c(1,         16,          17,           3,           4))
     }
 
     
     ##-----------------------------------------------------------------------------
     ## plot incremental TWR
-    if (grepl('i', plottype)) plot( plotxts(twriall) )
+    if (grepl('i', plottype)) plot( plotxts(twriall,
+                                            main=portfolioname) )
 
 
     ##-----------------------------------------------------------------------------
     ## alpha/beta plot
 
-    ## separate into 3 dataframes
-    abhold  <- rr[rr$key != 'portfolio' & rr$key != 'benchmark',]
-    abport  <- rr[rr$key == 'portfolio',]
-    abbench <- rr[rr$key == 'benchmark',]
-
-    rr$alpha <- NA
-    rr$beta  <- NA
     if (grepl('a', plottype) | grepl('b', plottype)) {
-        benchi <- as.numeric(twriall[, ncol(twriall)])
-        for (i in 1:ncol(twriall)) {
-            out <- alpha_beta(as.numeric(twriall[,i]), benchi, plot=FALSE)
-            rr[i,]$alpha <- out$alpha
-            rr[i,]$beta  <- out$beta
-        }
-
-        ## separate into 3 dataframes
-        rrhold  <- rr[rr$key != 'portfolio' & rr$key != 'benchmark',]
-        rrport  <- rr[rr$key == 'portfolio',]
-        rrbench <- rr[rr$key == 'benchmark',]
         
         ## create plot
         xrange <- range(rr$beta)
         xlim   <- c(min(xrange),
                     max(xrange) + 0.25*(max(xrange) - min(xrange)))
         ylim   <- range(rr$alpha)
-
-        with(rrhold, plotfit(beta, alpha, key, interval='noline',
+        with(rrhold, plotfit(beta, alpha, label, interval='noline',
                              xlimspec=xlim, ylimspec=ylim,
                              xlabel = 'beta',
                              ylabel = 'alpha'))
         ## add portfolio
         points(rrport$beta, rrport$alpha, col='red', pch=16)
         ## add benchmark
-        points(rrbench$beta, rrbench$alpha, col='magenta1', pch=17)
+        points(rrbench$beta, rrbench$alpha, col='blue', pch=17)
         ## add subtitle text
-        mtext('(solid red circle is portfolio; solid magenta triangle is benchmark)',
+        mtext('(portfolio = solid red circle; benchmark = solid blue triangle)',
               side=3, line=1, cex=1)
         
     }
+
+    ## add weights to rr
+    rr$weight <- c(weight, NA, NA)
+
+    ## any correlation
+    stats <- select(rr, twrcum, std, alpha, beta, weight)
+    pairsdf(stats)
 
     return(list(twri = twriall, summary=rr))
 }
