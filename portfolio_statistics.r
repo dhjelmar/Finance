268ec90e2572be9afa600299599ab9641454882e
dlhjel <dlhjel@gmail.com>
2021-11-23 12:35:35 -0500

    first working version

--- portfolio_statistics.r
+++ portfolio_statistics.r
@@ -2,245 +2,166 @@
 ## setup
 os <- .Platform$OS.type
 if (os == 'windows') {
-  ## load generic modules
-  source("F:\\Documents\\01_Dave's Stuff\\Programs\\GitHub_home\\R-setup\\setup.r")
-  ## identify working folder
-  path <- c("f:/Documents/01_Dave's Stuff/Programs/GitHub_home/Finance/")
+    ## load generic modules
+    source("F:\\Documents\\01_Dave's Stuff\\Programs\\GitHub_home\\R-setup\\setup.r")
+    ## identify working folder
+    path <- c("f:/Documents/01_Dave's Stuff/Programs/GitHub_home/Finance/")
 } else {
-  ## os == unix
-  source('~/GitHub_repos/R-setup/setup.r')
-  path <- c('~/GitHub_repos/Finance/')
+    ## os == unix
+    source('~/GitHub_repos/R-setup/setup.r')
+    path <- c('~/GitHub_repos/Finance/')
 }
 ## set working folder
 setwd(path)
 ## load local modules
 r_files <- list.files(paste(path, 'modules/', sep=''), pattern="*.[rR]$", full.names=TRUE)
 for (f in r_files) {
-  ## cat("f =",f,"\n")
-  source(f)
+    ## cat("f =",f,"\n")
+    source(f)
 }
 
 
 ##-----------------------------------------------------------------------------
 ## Import portfolio information
-## Only required info out of this section is to have a dataframe with:
-##       Symbol
-##       Weight
-## In this example, additional info is saved
 account_info <- readall("account_info.xlsx")
-account_info <- readall("F:\\Documents\\01_Dave's Stuff\\Finances\\allocation.xlsx",
-                       sheet="all assets", header.row = 2, data.start.row = 4)
-account_info$Account_Name <- account_info$Account
+
+## account_info <- readall("F:\\Documents\\01_Dave's Stuff\\Finances\\allocation.xlsx",
+##                        sheet="all assets", header.row = 2, data.start.row = 4)
+## account_info$Account_Name <- account_info$Account
+
+## strip out any lines with incomplete info
 account_info <- na.omit(account_info)
 
 ## yahoo uses "-" instead of "." or "/" in symbol names so convert
 account_info$Symbol <- gsub("\\.|\\/", "-", account_info$Symbol)
 
-## consider moneymarkets as cash since yahoo does not have info for them
-account_info[(account_info$Symbol == 'SWVXX' | account_info$Symbol == 'SWYXX'),]$Symbol <- 'Cash'
+## consider moneymarkets and individual bonds (long symbols) as 3-month treasury
+## since yahoo does not have info for them
+account_info[(account_info$Symbol == 'SWVXX' | account_info$Symbol == 'SWYXX'),]$Symbol <- 'SHV'
+account_info[nchar(account_info$Symbol) > 8,]$Symbol <- 'SHV'
 
-## do same with individual bonds
-account_info[nchar(account_info$Symbol) > 8,]$Symbol <- 'Cash'
+## yahoo does not have cash either, but added option to give 'Cash' a negligible return
+account_info[(account_info$Symbol == 'Cash & Cash Equivalents'),]$Symbol <- 'Cash'
 
-## maybe better to consider those things SHV (3-month treasury)
-account_info[(account_info$Symbol == 'Cash'),]$Symbol <- 'SHV'
+## strip to only what is needed to identify unique accounts
+account_info <- select(account_info, c('Account_Name', 'Owner', 'Account_Type', 'Symbol', 'Quantity'))
 
-## if too much was read in, strip to only what is needed to identify unique accounts
-account_info <- select(account_info, c('Account_Name', 'Owner', 'Account_Type', 'Symbol', 'Quantity', 'Weight'))
 
 ##-------------------------------------------------------------------------
-## SELECT ACCOUNT
-account <- split(account_info, account_info$Account_Name)
-names(account)
-df <- account$`DE Invest`
+## GET TWR AND CURRENT PRICES FOR EVERY HOLDING and TWR FOR BENCHMARK
 
-## EVALUATE
-holding <- df$Symbol
-twri    <- equitytwr(holding)
-twrib   <- equitytwr('SPY', period='months')
-out <- portfolio(holding,
-                 df$Weight,
-                 twri  = twri,
-                 twrib = twrib,
-                 from = '2020-10-30',
-                 to   = '2021-10-30',
-                 plottype = 'cria')
+## ## set period for twri (days, months, years)
+period     <- 'months'
+
+## holdings
+holdingall <- unique(account_info$Symbol)
+closeall   <- equityprice(holdingall)
+twriall    <- equitytwr(holdingall, period='month')
+
+## benchmark options
+efdata   <- ef('Schwab',        from='1900-01-01', to=Sys.Date(), addline=FALSE)
+efdata2  <- ef('S&P 500 / AGG', from='1900-01-01', to=Sys.Date(), addline=FALSE)
+eftwri   <- cbind(efdata$twri, efdata$eftwri, efdata2$eftwri)
 
 
-out <- portfolio(holding,
-                 df$Weight,
-                 twri  = twri,
-                 twrib = twrib,
-                 from = '2018-10-30',
-                 to   = '2021-10-30',
-                 plottype = 'ra')
 
-out$summary[order(out$summary$twrcum),]
+###########################################################################
+###########################################################################
 
+## restart from here to create a new portfolio for evaluation
 
-##-----------------------------------------------------------------------------
-##-----------------------------------------------------------------------------
+###########################################################################
+###########################################################################
 
-## Below is additional but probably now broken fanciness to update weights
-## based on quantities of each holding. Need to pull that into a separate function.
-
-##-----------------------------------------------------------------------------
-##-----------------------------------------------------------------------------
-## ADD PRICE FOR EACH SECURITY TO account_info
-
-## identify securities
-securitylist <- unique(account_info$Symbol)
-
-## drop money markets and individual bonds (long symbol names) since yahoo does not deal with them
-securitylist <- securitylist[! securitylist %in% c('SWVXX', 'SWYXX')]
-securitylist <- securitylist[nchar(securitylist) < 8]
-
-## obtain recent closing prices
-out1 <- equityprice(securitylist, refresh=TRUE, file='out1.csv')
-security <- data.frame(Symbol=securitylist, name=out1$Name, close=out1$Close)
-
-## Add recent price data to account_info
-account_info$Close <- security$close[match(account_info$Symbol, security$Symbol)]
-
-## change NA closing prices to 1.0 (i.e., bonds and money markets)
-temp <- account_info$Close
-temp[is.na(temp)] <- 1
-account_info$Close <- temp
-
-##-----------------------------------------------------------------------------
-## Get twr values for account securities
-alltwr <- equitytwr(securitylist, refresh=FALSE, file='out2.csv')
-
-## Get twr values for benchmark
-benchtwr <- equitytwr('SPY', refresh=FALSE, file='out3.csv')
-
-
-##---------------------------------------------------------------------------
-##---------------------------------------------------------------------------
-## Select unique account or combine accounts for evaluation
+##-------------------------------------------------------------------------
+## CREATE PORTFOLIO TO BE EVALUTED BY SELECTING ACCOUNT (or COMBINED ACCOUNT)
+## list account names, owners, and types
+unique(account_info$Account_Name)
+unique(account_info$Owner)
 unique(account_info$Account_Type)
-## split account info by Acount_Type into a list of dataframes
-account <- split(account_info, account_info$Account_Type)
-names(account)
 
-## select one to work with
-i <- 2
-dfname <- names(account)[i]
-print(dfname)
-df <- account[[i]]
+## ## this will create a list of all accounts
+## account <- split(account_info, account_info$Account_Name)
+## names(account)
+## portfolio <- account$`DE Invest`
 
-## ## alternately could have used subset to pull out a single account or combine accounts
-## invest <- subset(account_info, account_info$Account_Type == "Investment")
-## ira    <- subset(account_info, account_info$Account_Type == "IRA - Traditional")
-## dfname <- 'ira'
-## df     <- ira
+## select accounts to create portfolio and give it a name
+portfolioname <- 'Investment Account'
+portfolio <- subset(account_info, Account_Type == 'invest')
+portfolio <- select(portfolio, c('Symbol', 'Quantity'))
+names(portfolio) <- c('Holding', 'Quantity')
 
-##---------------------------------------------------------------------------
-##---------------------------------------------------------------------------
-## combine duplicate symbol entries if needed and drop columns except for Symbol and Quantity
-df <- aggregate(df$Quantity, by=list(df$Symbol, df$Close), FUN=sum)
-names(df) <- c('Symbol', 'Close', 'Quantity')
-
-## ## convert data to vectors
-## asset   <- as.character(df$Symbol)
-## shares  <- df$Quantity
+## ## print portfolio to be evaluated
+## print(portfolio, 9999)
 
 
-##-----------------------------------------------------------------------------
-## GIVEN DATAFRAME WITH SYMBOL, CLOSING VALUE, AND QUANTITY OF THAT SYMBOL
-## EVALUTATE THE SYBMOLS AND PORTFOLIO
+##-------------------------------------------------------------------------
+## COLLAPSE IDENTICAL HOLDINGS (ESPECIALLY FOR COMBINED ACCOUNTS)
 
-stopped here
-
-for (asset in df$Symbol) {
-  out <- equityeval(asset, 'SPY')
-  duration <- out$duration
-alpha <- out$alpha
-beta  <- out$beta
-twrcum <- out$twrcum
-benchcum <- out$benchcum
-}
-
-out <- equitymodel('AAPL', 'SPY')
-out <- equityeval('AAPL', 'SPY')
-DT::datatable(signif(out,4))
+## combine duplicate holdings
+portfolio <- aggregate(portfolio$Quantity, by=list(portfolio$Holding), FUN=sum)
+names(portfolio) <- c('Holding', 'Quantity')
 
 
-out <- equityeval('IFED', 'SPY', period='days', duration='1 year')
-out <- equityeval('IFED', 'SPY', period='days', duration='1 year', from='2021-10-11')
+##-------------------------------------------------------------------------
+## DETERMINE WEIGHT OF EACH HOLDING
 
+## add closing price data to portfolio
+closeall$Holding <- rownames(closeall)
+portfolio <- merge(portfolio, closeall, by='Holding')
+portfolio <- select(portfolio, c('Holding', 'Name', 'Quantity', 'Close'))
 
-outzoo <- zoo::zoo(out)
-outzoo$duration_years <- NULL
-zoo::index(outzoo) <- c(1,3,5)
-zoo::plot.zoo(outzoo)
+## determine weight
+portfolio$Market_Value <- portfolio$Quantity * portfolio$Close
+totalvalue             <- sum(portfolio$Market_Value)
+portfolio$Weight       <- portfolio$Market_Value / totalvalue
 
+## print(portfolio)
 
+##-------------------------------------------------------------------------
+## GET TWRI FOR PORTFOLIO FROM TWRIALL
+twri <- twriall[, (colnames(twriall) %in% portfolio$Holding)]
+times <- zoo::index(twri)
 
+## reorder to match order in portfolio dataframe
+## first convert to dataframe
+twri <- as.data.frame(twri)
+## reorder to match portfolio
+twri <- select(twri, portfolio$Holding)
+## convert back to xts
+twri <- xts::as.xts(twri)
+## above did not recover the exact same timestamp so replacing
+zoo::index(twri) <- times
 
-df <- xts::last(df, '5 years')
+##-------------------------------------------------------------------------
+## EVALUATE PORTFOLIO
+eftwri$schwab_70_30 = (eftwri$schwab_60_40 + eftwri$schwab_80_20)/2
+twrib <- eftwri$schwab_70_30
+out <- portfolio_eval(portfolio$Holding,
+                      portfolio$Weight,
+                      twri  = twri,
+                      twrib = twrib,
+                      from = '2020-10-30',
+                      to   = '2021-10-30',
+                      plottype = 'cria',
+                      portfolioname = portfolioname)
 
+summary <- out$summary
+summary[order(summary$twrcum),]
 
-
-## df = dataframe containing Symbol, Close, and Quantity
-## alltwr = XTS object with historical TWR for all symbols in df and possibly more
-## benchtwr = XTS object with historical TWR for benchmark
-
-## first determine weights of each asset
-df$Value <- df$Close * df$Quantity
-totalvalue <- sum(df$Value)
-df$weight <- df$Value / totalvalue
-
-
-
-
-
-stats <- data.frame(symbol,
-                    name = symbolname,
-                    shares = as.numeric(shares), 
-                    value=as.numeric(value), 
-                    twr  = twri,
-                    beta, 
-                    alpha)
-## sort from low to high twr
-stats <- stats[order(stats$twr),]
-
-
-## calculate portfolio beta
-beta_portfolio  <- sum( weight * beta  )
-alpha_portfolio <- sum( weight * alpha )
-twr_portfolio   <- sum( weight * twri  )
-portfolio <- data.frame(symbol  = dfname, 
-                        name   = 'portfolio',
-                        shares = NA, 
-                        value  = totalvalue, 
-                        twr    = twr_portfolio,
-                        beta   = beta_portfolio,
-                        alpha  = alpha_portfolio)
-stats <- rbind(stats, portfolio)
-print(stats)
-
-## plot portfolio
-plotspace(1,2)
-out <- plotfit(stats$alpha, stats$beta, stats$symbol, nofit=TRUE)
-## xx <- stats[nrow(stats),]$beta
-## yy <- stats[nrow(stats),]$alpha
-## color <- as.character(out$legend[nrow(out$legend),]$color)
-## points(xx, yy, pch=16, col=color)
-out <- plotfit(stats$alpha, stats$twr, stats$symbol, nofit=TRUE)
-
-## any correlation between alpha, beta, and twr?
-abtwr <- select(stats, twr, alpha, beta)
-pairsdf(abtwr)
+portfolio <- merge(portfolio, summary, by='Holding')
+portfolio$weight <- NULL
+portfolio$label  <- NULL
+printdf(portfolio, 99)
 
 ## ## plot interactive
 ## if (os == 'windows') {
 ##     ## following uses plotly which does not work on Chromebook
-##     plot_interactive(stats, 'beta', 'alpha')
+##     plot_interactive(rr, 'std', 'twrcum')
+##     plot_interactive(rr, 'beta', 'alpha')
 ## }
 
 library(shiny)
-shinyplot(as.data.frame(stats), 'beta', 'alpha')
-}
-
+shinyplot(as.data.frame(summary), 'std', 'twrcum')
+shinyplot(as.data.frame(summary), 'beta', 'alpha')
 
