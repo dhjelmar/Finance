d4aeb545d95a4284350fc0f607a8f059c1fddfb5
dlhjel <dlhjel@gmail.com>
2021-11-24 10:02:42 -0500

    initial commit

---
+++ modules/porttwri.r
@@ -0,0 +1,24 @@
+porttwri <- function(twriall, holding) {
+    ## input:   twriall = xts object with twri for many possible holdings
+    ##                    index contains dates
+    ##                    column for each possible holding symbol
+    ##          holding = vector of holding symbols
+    ## output:  twri    = xts object with twri for holding symbols only
+
+    ## GET TWRI FOR PORTFOLIO FROM TWRIALL
+    ## all_of() function used to silance a warning message
+    twri <- twriall[, (colnames(twriall) %in% tidyselect::all_of(holding))]
+    times <- zoo::index(twri)
+
+    ## reorder to match order in portfolio dataframe
+    ## first convert to dataframe
+    twri <- as.data.frame(twri)
+    ## reorder to match portfolio
+    twri <- select(twri, holding)
+    ## convert back to xts
+    twri <- xts::as.xts(twri)
+    ## above did not recover the exact same timestamp so replacing
+    zoo::index(twri) <- times
+
+    return(twri)
+}
---
+++ modules/weights.r
@@ -0,0 +1,30 @@
+weights <- function(portfolio, portfolioname=NULL) {
+    ## input:  portfolio = dataframe with columns labeled "Holding" and "Quantity"
+    ## output: portfolio with added columns for "Market_Value" and "Weight"
+    
+    ##-------------------------------------------------------------------------
+    ## COLLAPSE IDENTICAL HOLDINGS (ESPECIALLY FOR COMBINED ACCOUNTS)
+
+    ## combine duplicate holdings
+    portfolio <- aggregate(portfolio$Quantity, by=list(portfolio$Holding), FUN=sum)
+    names(portfolio) <- c('Holding', 'Quantity')
+
+
+    ##-------------------------------------------------------------------------
+    ## DETERMINE WEIGHT OF EACH HOLDING
+
+    ## obtain closing price data and merge with portfolio
+    closeall   <- equityprice(portfolio$Holding)
+    closeall$Holding <- rownames(closeall)
+    portfolio <- merge(portfolio, closeall, by='Holding')
+    portfolio <- select(portfolio, c('Holding', 'Name', 'Quantity', 'Close'))
+
+    ## determine weight
+    portfolio$Market_Value <- portfolio$Quantity * portfolio$Close
+    totalvalue             <- sum(portfolio$Market_Value)
+    portfolio$Weight       <- portfolio$Market_Value / totalvalue
+
+    ## print(portfolio)
+
+    return(portfolio)
+}
