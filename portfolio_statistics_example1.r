28abdf2d2985a102621a1b1c2013b3ba40b7acff
dlhjel <dlhjel@gmail.com>
2021-12-02 08:28:02 -0500

    initial commit

---
+++ portfolio_statistics_example1.r
@@ -0,0 +1,50 @@
+##-----------------------------------------------------------------------------
+## setup
+os <- .Platform$OS.type
+if (os == 'windows') {
+    ## load generic modules
+    source("F:\\Documents\\01_Dave's Stuff\\Programs\\GitHub_home\\R-setup\\setup.r")
+    ## identify working folder
+    path <- c("f:/Documents/01_Dave's Stuff/Programs/GitHub_home/Finance/")
+} else {
+    ## os == unix
+    source('~/GitHub_repos/R-setup/setup.r')
+    path <- c('~/GitHub_repos/Finance/')
+}
+## set working folder
+setwd(path)
+## load local modules
+r_files <- list.files(paste(path, 'modules/', sep=''), pattern="*.[rR]$", full.names=TRUE)
+for (f in r_files) {
+    ## cat("f =",f,"\n")
+    source(f)
+}
+
+##-----------------------------------------------------------------------------
+## identify holdings and weights that comprise portfolio
+portfolioname <- 'My Portfolio'
+holding       <- c('PTNQ', 'SPYG', 'VOOG', 'JPST', 'LALDX', 'PONAX', 'SPLV')
+weight        <- rep(1/length(holding), length(holding))
+
+## define a benchmark to use in the evaluation
+twrib   <- 'SPY'
+
+## define a timeframe and period for incremental TWR
+period   <- 'months'
+from     <- '2018-10-31'
+to       <- '2021-11-30'
+duration <- paste(from, 'to', to, sep=' ')
+
+## evaluate portfolio
+out <- portfolio_eval(holding, weight=weight, twrib=twrib,
+                      plottype = c('twrcum', 'rr', 'twri', 'ab'),
+                      from=from, to=to, period=period,
+                      main = paste(portfolioname, '; duration =', duration, sep=' '))
+
+## look at correlation between investments
+pairsdf(as.data.frame(out$twri))
+
+##-----------------------------------------------------------------------------
+## DEEP DIVE INTO SPECIFIC HOLDING
+out <- equityeval('BKI', 'SPY', duration='3 years')
+out
