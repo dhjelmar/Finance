c535e3ad87ff4f94ec9cd0feba4f385a78a0a82b
dlhjel <dlhjel@gmail.com>
2021-12-04 11:22:43 -0500

    added option to pass in twri and twrib and improved ability to use specified from/to dates

--- modules/equity.eval.r
+++ modules/equity.eval.r
@@ -1,21 +1,45 @@
 equity.eval <- function(symbol, bench, period='months', from=NULL, to=NULL,
                        duration = c('1 year', '3 years', '5 years'),
-                       makeplots = TRUE) {
+                       makeplots = TRUE,
+                       twri = NULL, twrib = NULL) {
 
     ## given a single symbol and benchmark, creates:
-    ##     dataframe containing duration, twrcum, alpha, beta, stdev, sharpe, benchcum, benchstdev, benchsharpe
-    ##     set of plots of: increemntal TWR, 
+    ##     dataframe containing duration, twrcum, alpha, beta, stdev, sharpe, twrcumb, stdevb, sharpeb
+    ##     set of plots of: incremental TWR, 
     ##                      cumulative twr (twrcum), 
     ##                      incremental twr for symbol vs. benchmark,
     ##                      cumulative twr vs. standard deviation for symbol and benchmark
-  
-    ## get history
-    out <- equity.history(symbol, period=period, from=from, to=to)  # 50 works, 60 does not
-    twr <- out$twr
-    benchtwr <- equity.history(bench, period=period)$twr
+
+    ## options: from and to are used to define the maximum date range
+    ##          duration = c('1 year', '3 years', '5 years') default makes plots for all 3
+    ##                     can specify 1, 2 or all 3 of these as options
+    ##                   = number instead returns the last this number of time steps
+    ##                     Use this option with from/to to set a specific date range, e.g.:
+    ##                         from='2020-10-28', to='2020-12-31', period='days', duration=99999 
     
-    ## combine twr and benchmarks to line up dates and remove NA
-    both <- na.omit( cbind(twr, benchtwr) )
+    ## get history
+    if (is.null(twri)) {
+        ## twri not passed in so obtain
+        out  <- equity.history(symbol, period=period, from=from, to=to)
+        twri <- out$twri
+    }
+    if (is.null(twrib)) {
+        ## twrib not passed in so obtain (no need to worry about dates since will line up later)
+        twrib <- equity.history(bench, period=period)$twri
+    } else {
+        ## twrib is passed in
+        twrib <- twrib
+    }
+
+    if (!is.null(from) | is.null(to)) {
+        ## from and/or to are not defined so do not use to restrict date range
+        xtsrange <- paste(noquote(from), '/', noquote(to), sep='')
+        xtsrange
+        twri <- twri[xtsrange]
+    }
+        
+    ## combine twri and benchmarks to line up dates and remove NA
+    both <- na.omit( cbind(twri, twrib) )
 
     ## set plotspace to fill by columns first
     par(mfcol=c(3, length(duration)))
@@ -24,11 +48,11 @@ equity.eval <- function(symbol, bench, period='months', from=NULL, to=NULL,
     alpha    <- NA
     beta     <- NA
     twrcum   <- NA
-    benchcum <- NA
+    twrcumb <- NA
     stdev    <- NA
-    benchstdev <- NA
+    stdevb <- NA
     sharpe   <- NA
-    benchsharpe <- NA
+    sharpeb <- NA
     
     i        <- 0
     for (durationi in duration) {
@@ -36,18 +60,18 @@ equity.eval <- function(symbol, bench, period='months', from=NULL, to=NULL,
       
         ## extract vectors for alapha and beta
         bothx <- xts::last(both, durationi)
-        twrx       <- as.numeric( bothx[, 1] )
-        benchtwrx  <- as.numeric( bothx[, 2] )
+        twrix       <- as.numeric( bothx[, 1] )
+        twribx  <- as.numeric( bothx[, 2] )
         
         ## calculate cumulative returns for durationi
-        twrcum[i]   <- prod(twrx + 1) - 1
-        benchcum[i] <- prod(benchtwrx + 1) - 1
+        twrcum[i]   <- prod(twrix + 1) - 1
+        twrcumb[i] <- prod(twribx + 1) - 1
 
         ## calculate standard deviation and sharpe ratio for durationi
-        stdev[i]       <- sd(twrx)
-        benchstdev[i]  <- sd(benchtwrx)
+        stdev[i]       <- sd(twrix)
+        stdevb[i]  <- sd(twribx)
         sharpe[i]      <- twrcum[i]   / stdev[i]
-        benchsharpe[i] <- benchcum[i] / benchstdev[i]
+        sharpeb[i] <- twrcumb[i] / stdevb[i]
 
         ## plot incremental and cumulative returns for input duration
         xtscum <- cumprod(bothx+1)-1
@@ -57,23 +81,25 @@ equity.eval <- function(symbol, bench, period='months', from=NULL, to=NULL,
             
             ## add cumulative TWR to plot
             mtext(paste('TWR Cum = ', signif(twrcum[i],4)*100, '%;',
-                        'Benchmark Cum = ', signif(benchcum[i], 4)*100, '%',
+                        'Benchmark Cum = ', signif(twrcumb[i], 4)*100, '%',
                         sep=''), 
                   side=3, line=1, cex=0.75)
         }
 
         ## determine alpha and beta for symbol i and create plot
-        out <- alpha.beta(twrx, benchtwrx, 
+        out <- alpha.beta(twrix, twribx, 
                           plot = makeplots, 
                           xlabel = paste('Incremental TWR for', bench, sep=' '),
                           ylabel = paste('Incremental TWR for', symbol, sep=' '),
-                          range  = range(twrx, benchtwrx, na.rm = TRUE),
+                          range  = range(twrix, twribx, na.rm = TRUE),
                           main   = durationi)
         alpha[i] <- out$alpha
         beta[i]  <- out$beta
         
     }
     df  <- data.frame(duration, twrcum, alpha, beta, stdev, sharpe,
-                      benchcum, benchstdev, benchsharpe)
+                      twrcumb, stdevb, sharpeb)
     return(df)
 }
+## out <- equity.eval('AAPL', 'SPY', from='2020-10-11', to='2020-12-15',
+##                   period='days', duration=99999 )
