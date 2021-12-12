7a1785339b77b88acec221539475c0125be065e8
dlhjel <dlhjel@gmail.com>
2021-12-12 09:45:04 -0500

    simplified loop to make more use of twri_list

--- glassdoor.r
+++ glassdoor.r
@@ -38,25 +38,11 @@ year    hold1   hold2   hold3   hold4   hold5   hold6   hold7   hold8   hold9   
 2020    HUBS    BCSF    DOCU    ISRG    LUV     GOOGL   NVDA    MSFT    LULU    CPT
 2021    BCSF    NVDA    HUBS    GOOGL   DAL     LULU    MSFT    SYK     DOCU    CRM   # META and KNBE in top 10 but not public until 6/2021 and 5/2021
 '
-
-shares <- '
-year    hold1   hold2   hold3   hold4   hold5   hold6   hold7   hold8   hold9   hold10
-2009    51      235     44      7       34      84      46      26      24      31
-2010    151     66      37      68      103     5       56      11      27      46
-2011    150     76      112     40      13      94      183     64      40      69
-2012    5       407     130     57      65      23      209     79      35      39
-2013    6       93      202     26      42      47      38      22      337     47
-2014    44      45      61      6       39      51      33      30      42      42
-2015    6       24      37      42      51      75      42      35      123     37
-2016    54      57      5       127     55      27      34      70      128     137
-2017    5       32      31      110     41      47      79      30      72      49
-2018    341     5       56      50      42      43      83      112     69      90   # incorrectly assumes BCSF and DOCU
-2019    364     76      5       39      102     35      31      38      116     80
-2020    43      427     90      35      125     5       115     44      30      69   # incorrectly assumes ZM
-2021    960     83      28      7       279     31      50      715     46      49   # incorrectly assumes META
-'
-
 holding <- readall(holdings)
+
+## ## work with subset for debugging purposes
+## holding <- head(holding[,1:3], 2)
+
 nhold   <- ncol(holding)-1
 
 ## equal weight for each holding (units = fraction of total portfolio value)
@@ -64,28 +50,27 @@ weight   <- rep(1/nhold, nhold)
 
 ## starting value of portfolio and period for each twri value
 value_last <- 10000
-period     <- 'months'
+period     <- 'days'
 
 ## read in twri for portfolio if desired and available
 refresh <- TRUE
 if (isFALSE(refresh)) {
-  twri_in <- readall('glassdoor_twri.csv')
-  rownames(twri_in) <- twri_in$X
-  twri_in$X <- NULL
+  twri.csv <- readall('glassdoor_twri.csv')
+  rownames(twri.csv) <- twri.csv$X
+  twri.csv$X <- NULL
   ## change dataframe to xts object
-  twri_in <- xts::as.xts(twri_in)
+  twri.csv <- xts::as.xts(twri.csv)
   ## set dates consistent with how zoo and xts want them
-  zoo::index(twri_in) <- as.Date( zoo::index(twri_in))
+  zoo::index(twri.csv) <- as.Date( zoo::index(twri.csv))
+
+  twri.rds <- readRDS('glassdoor_twri.rds')
 }
 
 ## initialize xts objects
 twri_list <- NA
-twri  <- xts::xts(t(rep(NA, nhold+1)), order.by=as.Date('2008-12-31'))
-names(twri) <- c(names(holding[1,2:(nhold+1)]), 'portfolio')
-twrc  <- twri
-value <- xts::xts(t( c( rep(value_last/nhold, nhold), value_last) ),
-                  order.by=as.Date('2008-12-31'))
-names(value) <- names(twri)
+twri      <- NA
+twrc      <- NA
+value     <- NA
 
 ## evaluate portfolio
 for (i in 1:nrow(holding)) {
@@ -94,13 +79,19 @@ for (i in 1:nrow(holding)) {
     yeari <- as.character(holding[i,1])
     cat('year = ', yeari, '\n')
 
+    ## starting and ending dates for yeari
+    from <- paste(holding[i,1] - 1, '-12-31', sep='')
+    to   <- paste(holding[i,1]    , '-12-31', sep='')
+    xtsrange <- paste(noquote(from), '/', noquote(to), sep='')
+
     ## obtain xts object of holdings
     holding.yeari <- as.character(holding[i, 2:(nhold+1)])
     if (isTRUE(refresh)) {
         twri.yeari <- equity.twri(holding.yeari, period=period)
     } else {
-        twri.yeari <- twri_in[yeari][,1:nhold]
+        twri.yeari <- twri.csv[,1:nhold]   # 1:nhold needed to not include "portfolio"
     }
+    twri.yeari <- twri.yeari[xtsrange]
 
     ## ## until list of holdings gets fixed, set NA to zero
     ## twri.yeari[is.na(twri.yeari)] <- 0
@@ -120,21 +111,27 @@ for (i in 1:nrow(holding)) {
     ## calculate value of portfolio
     value.yeari <- twrc.yeari * value_last
     
-    ## catonate results to those from  prior year
-    twri  <- rbind(twri,   twri.yeari[yeari])
-    twrc  <- rbind(twrc,   twrc.yeari[yeari])
-    value <- rbind(value, value.yeari[yeari])
-
     ## resest portfolio value to be used for next year purchases and growth
     value_last  <- value.yeari[nrow(value.yeari),]$portfolio[[1]]
 
 }
-## remove 1st NA line
-twri <- twri[-1,]
+
+## create twri xts object containing all years
+##    twri <- data.table::rbindlist(twri_list)  # only works for dataframe
+twri <- do.call(rbind, twri_list)  # works for matrices (and xts)
+## eliminate 2nd occurance of same times
+## (eliminate 2nd since each list starts with end of prior year but for new holdings)
+twri <- twri[!duplicated(time(twri)),]
+## replace names since each holding changes every year
+names(twri) <- c(names(holding)[2:ncol(holding)], 'portfolio')
+
 dates <- zoo::index(twri)
 if (isTRUE(refresh)) {
     twridf <- as.data.frame(dates=dates, twri)
     write.csv(twridf, 'glassdoor_twri.csv')
+
+    saveRDS(twri, 'glassdoor_twri.rds')
+    saveRDS(twri_list, 'glassdoor_twri_list.rds')
 }
 
 ## print start of each year to make sure all selected securities are available
@@ -159,7 +156,7 @@ std     <- t( as.matrix( apply(twri[2:nrow(twri),], 2, sd, na.rm=TRUE) ) )
 ##     plot( plotxts(twrc.yeari) )
 ## }
 
-createpdf <- FALSE
+createpdf <- TRUE
 if (isTRUE(createpdf)) pdf(file = "glassdoor.pdf", onefile = TRUE,          # creates a multi-page PDF file
                            ## file = "glassdoor%03d.pdf", onefile = FALSE,  # creates multiple PDF files
                            width = 9,  # The width of the plot in inches
@@ -176,10 +173,10 @@ out <- portfolio.eval(names(holding)[2:(nhold+1)], weight=weight, twri=twri, twr
                       main = paste(portfolioname, '; duration =', duration, sep=' '))
 
 ## evaluate portfolio as if it was a mutual fund
-out <- equity.eval(portfolioname, 'SPY', twri=twri$portfolio)
+out <- equity.eval(portfolioname, 'SPY', twri=twri$portfolio, period=period)
 
 ## plot performance during each year of portfolio
-twrib <- equity.twri('SPY')
+twrib <- equity.twri('SPY', period=period)
 for (i in 1:nrow(holding)) {
     yeari <- as.character(holding[i,1])
     cat('Creating plots for year =', yeari, '\n')
@@ -197,3 +194,22 @@ for (i in 1:nrow(holding)) {
 }
 
 if (isTRUE(createpdf)) dev.off() # close external pdf (or jpg) file
+
+
+
+## shares <- '
+## year    hold1   hold2   hold3   hold4   hold5   hold6   hold7   hold8   hold9   hold10
+## 2009    51      235     44      7       34      84      46      26      24      31
+## 2010    151     66      37      68      103     5       56      11      27      46
+## 2011    150     76      112     40      13      94      183     64      40      69
+## 2012    5       407     130     57      65      23      209     79      35      39
+## 2013    6       93      202     26      42      47      38      22      337     47
+## 2014    44      45      61      6       39      51      33      30      42      42
+## 2015    6       24      37      42      51      75      42      35      123     37
+## 2016    54      57      5       127     55      27      34      70      128     137
+## 2017    5       32      31      110     41      47      79      30      72      49
+## 2018    341     5       56      50      42      43      83      112     69      90   # incorrectly assumes BCSF and DOCU
+## 2019    364     76      5       39      102     35      31      38      116     80
+## 2020    43      427     90      35      125     5       115     44      30      69   # incorrectly assumes ZM
+## 2021    960     83      28      7       279     31      50      715     46      49   # incorrectly assumes META
+## '
