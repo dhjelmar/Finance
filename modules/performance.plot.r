49d81fce90b497c4ae7b07944b22d8d72e936273
dhjelmar <dlhjel@gmail.com>
2021-12-27 12:49:18 -0500

    initial commit; pulled coding from performance.r and moved into functions

---
+++ modules/performance.plot.r
@@ -0,0 +1,54 @@
+performance.plot <- function(portfolio, valuesheet, twrsheet, twrib, xtsrange, period,
+                             portfolioname = NULL, file = NULL) {
+    
+    
+    ## determine weight of each portion of portfolio
+    value         <- valuesheet[,names(valuesheet) %in% portfolio]
+    current.value <- as.numeric( tail(value, 1) )
+    weight        <- current.value / sum(current.value)
+
+    if (!is.null(file)) pdf(file = file, onefile = TRUE,          # creates a multi-page PDF file
+                            ## file = "performance%03d.pdf", onefile = FALSE,  # creates multiple PDF files
+                            width = 9,  # The width of the plot in inches
+                            height = 7) # The height of the plot in inches
+
+
+    ## create value plot
+    plotspace(3,1)
+    plot( plotxts(value[xtsrange]) )
+
+    ## extract actual from and to to be used in plots based on applying xtsrange
+    xts  <- twrsheet[xtsrange]
+    from <- zoo::index(xts[1,])
+    to   <- zoo::index(xts[nrow(xts),])
+    duration <- paste(from, 'to', to, sep=' ')
+    if (is.null(portfolioname)) portfolioname <- 'Portfolio'
+    main <- paste(portfolioname, ': ', duration, '; period = ', period, sep='')
+    cat('\n', main, '\n\n')
+
+    ## create cumulative and incremental TWR plots
+    ## period = days used in the following so there is a unique value for every date read from Excel
+    ## all date entries are consisered equally in evaluation of standard deviation, alpha, and beta
+    out1 <- portfolio.eval(portfolio, weight=weight, twri=twrsheet, twrib='SPY',
+                           plottype = c('twrc', 'twri'), arrange=FALSE,
+                           from=from, to=to, period=period,
+                           main = main)
+
+    ## create risk/return plot
+    out2 <- portfolio.eval(portfolio, weight=weight, twri=twrsheet, twrib='SPY',
+                           plottype = c('rr', 'ab'),
+                           from=from, to=to, period=period,
+                           main = main)
+    out2$performance$value <- c(current.value, sum(current.value), NA)
+    
+    ## evaluate portfolio as if it was a mutual fund
+    twri <- twrsheet[,names(twrsheet) %in% portfolio]
+    ## out <- equity.eval(portfolioname, 'SPY', twri=out$twri$portfolio, period=period)
+    out3 <- equity.eval(portfolioname, 'SPY', twri=twri, period=period)
+
+
+    if (!is.null(file)) dev.off() # close external pdf (or jpg) file
+
+    return(list(out.xtsrange=out2, out.125yrs=out3))
+
+}
--- performance.r
+++ performance.r
@@ -8,7 +8,7 @@
 ## setup
 path <- getwd()
 source(paste( strsplit( getwd(), 'Finance' ), '/R-setup/setup.r', sep='' ) )
-r_files <- list.files(paste(path, '/ modules/', sep=''), pattern="*.[rR]$", full.names=TRUE)
+r_files <- list.files(paste(path, '/modules/', sep=''), pattern="*.[rR]$", full.names=TRUE)
 for (f in r_files) {
     ## cat("f =",f,"\n")
     source(f)
@@ -16,7 +16,6 @@ for (f in r_files) {
 
 ##-----------------------------------------------------------------------------
 ## READ DATA and CONVERT TO XTS
-source('performance.read.r')
 out        <- performance.read()
 map        <- out$map
 valuesheet <- out$valuesheet
@@ -31,6 +30,7 @@ church <- accounts[1:5]
 de     <- accounts[grepl('^D |^E |^DE', accounts)]
 p      <- accounts[grepl('^P'         , accounts)]
 
+##-----------------------------------------------------------------------------
 ## select a portfolio and timeframe for the evaluation
 portfolio     <-  church
 portfolioname <- 'Church'
@@ -41,18 +41,36 @@ portfolioname <- 'DE'
 portfolio     <- verify
 portfolioname <- 'verify01'
 
-
-from          <- '2020-12'
-to            <- '2021-12'
 period        <- 'months'
-xtsrange      <- paste(from, '/' , to, sep='')
+xtsrange <- '2020-12/2021-12'
 
-out <- performance.plot(portfolio, valuesheet, twrsheet, twrib, xtsrange,
+##-----------------------------------------------------------------------------
+## evaluate portfolio
+out <- performance.plot(portfolio, valuesheet, twrsheet, twrib, xtsrange, period,
                         portfolioname)
-performance <- as_tibble( out$out.xtsrange$performance )
-performance
+perf <- as_tibble( out$out.xtsrange$performance )  # tibble conversion strips the rownames
+perf
 
+df <- perf
+df[2:4]   <- sapply(df[2:4], function(x) scales::percent(x, accuracy=0.01))
+df$beta   <- round(df$beta  , 3)
+df$weight <- round(df$weight, 2)
+df$value  <- scales::dollar(df$value)
+## print as data frame rather than tibble so right justifies
+print( as.data.frame(df) )
 
+dlh
+something is not correct:
+  given: xtsrange  <- '2020-12/2021-12'
+         period    <- 'months'
+         portfolio <- church
+  portfolio has correct twrcum for accounts but not portfolio
+         14.96% in consolidated report
+         14.97% in Octave
+         15.09% in R
+
+         
+## same as above but creates pdf file with plot output
 out <- performance.plot(portfolio, valuesheet, twrsheet, twrib, xtsrange,
                         portfolioname, 
                         file = paste('performance.', portfolioname, '.pdf', sep=''))
