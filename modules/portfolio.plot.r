1dc2bceb29309431311de139d80e06fd7c174ac4
dhjelmar <dlhjel@gmail.com>
2022-01-28 16:21:36 -0500

    changed default pch for holdings to solid circles so can better tell color

--- modules/portfolio.plot.r
+++ modules/portfolio.plot.r
@@ -1,5 +1,5 @@
 portfolio.plot <- function(twri=NA, twrc=NA, perf=NA, twri.ef=NA,
-                           plottype=c('twri', 'twrc', 'rra', 'ab'),
+                           plottype=c('twri', 'twrc', 'rra', 'ab'), pch.hold=21,
                            main=NULL, label='symbol') {
 
     ## input: twri = incremental TWR (Time Weighted Return)
@@ -103,26 +103,28 @@ portfolio.plot <- function(twri=NA, twrc=NA, perf=NA, twri.ef=NA,
             efdata.schwab <- ef(model='Schwab', efdata=twri.ef, annualize=FALSE, addline=FALSE)
             efdata.simple <- ef(model='simple', efdata=twri.ef, annualize=FALSE, addline=FALSE)
             xrange <- range(perf$std, efdata.schwab$ef$efstd, efdata.simple$ef$efstd)
-            xlim   <- c(min(xrange),
-                        max(xrange) + 0.25*(max(xrange) - min(xrange)))
-            ylim   <- range(perf$twrc, efdata.schwab$ef$eftwrc, efdata.simple$ef$eftwrc)
+            xlim   <- c(min(xrange, na.rm = TRUE),
+                        max(xrange, na.rm = TRUE) + 
+                            0.25*(max(xrange, na.rm = TRUE) - min(xrange, na.rm = TRUE)))
+            ylim   <- range(perf$twrc, efdata.schwab$ef$eftwrc, efdata.simple$ef$eftwrc,
+                            na.rm = TRUE)
 
             ## plot holdings
             with(perfhold, plotfit(std, twrc, label, interval='noline',
-                                   xlimspec=xlim, ylimspec=ylim,
+                                   xlimspec=xlim, ylimspec=ylim, pch=pch.hold,
                                    xlabel = 'Standard Deviation',
                                    ylabel = 'Cumulative TWR',
                                    main   = main))
             ## add portfolio
-            points(perfport$std, perfport$twrc, col='red', pch=16)
+            points(perfport$std, perfport$twrc, col='red', pch=1)
             ## add benchmark (only the 1st)
-            points(perfbench1$std, perfbench1$twrc, col='blue', pch=17)
+            points(perfbench1$std, perfbench1$twrc, col='blue', pch=2)
             ## add efficient frontier line
             efdata.Schwab <- ef(model='Schwab', efdata=twri.ef, annualize=FALSE,
                                 addline=TRUE, col='black', lty=1, pch=3)
             efdata.simple <- ef(model='simple', efdata=twri.ef, annualize=FALSE,
                                 addline=TRUE, col='black', lty=2, pch=4)
-            mtext('(portfolio = solid red circle; benchmark = solid blue triangle)',
+            mtext('(portfolio = open red circle; benchmark = open blue triangle)',
                   side=3, line=0.8, cex=1)
             mtext('(Schwab EF = solid line; S&P 500 / AGG EF = dotted line)',
                   side=3, line=0, cex=1)
@@ -138,26 +140,28 @@ portfolio.plot <- function(twri=NA, twrc=NA, perf=NA, twri.ef=NA,
             efdata.schwab <- ef(model='Schwab', efdata=twri.ef, annualize=TRUE, addline=FALSE)
             efdata.simple <- ef(model='simple', efdata=twri.ef, annualize=TRUE, addline=FALSE)
             xrange <- range(perf$std.ann, efdata.schwab$ef$efstd, efdata.simple$ef$efstd)
-            xlim   <- c(min(xrange),
-                        max(xrange) + 0.25*(max(xrange) - min(xrange)))
-            ylim   <- range(perf$twrc.ann, efdata.schwab$ef$eftwrc, efdata.simple$ef$eftwrc)
+            xlim   <- c(min(xrange, na.rm = TRUE),
+                        max(xrange, na.rm = TRUE) + 
+                            0.25*(max(xrange, na.rm = TRUE) - min(xrange, na.rm = TRUE)))
+            ylim   <- range(perf$twrc.ann, efdata.schwab$ef$eftwrc, efdata.simple$ef$eftwrc,
+                            na.rm = TRUE)
 
             ## plot holdings
             with(perfhold, plotfit(std.ann, twrc.ann, label, interval='noline',
-                                   xlimspec=xlim, ylimspec=ylim,
+                                   xlimspec=xlim, ylimspec=ylim, pch=pch.hold,
                                    xlabel = 'Standard Deviation * sqrt(12)',
                                    ylabel = 'Annualized Cumulative TWR',
                                    main   = main))
             ## add portfolio
-            points(perfport$std.ann, perfport$twrc.ann, col='red', pch=16)
+            points(perfport$std.ann, perfport$twrc.ann, col='red', pch=1)
             ## add benchmark
-            points(perfbench1$std.ann, perfbench1$twrc.ann, col='blue', pch=17)
+            points(perfbench1$std.ann, perfbench1$twrc.ann, col='blue', pch=2)
             ## add efficient frontier line
             efdata.Schwab <- ef(model='Schwab', efdata=twri.ef,
                                 annualize=TRUE, addline=TRUE, col='black', lty=1, pch=3)
             efdata.simple <- ef(model='simple', efdata=twri.ef,
                                 annualize=TRUE, addline=TRUE, col='black', lty=2, pch=4)
-            mtext('(portfolio = solid red circle; benchmark = solid blue triangle)',
+            mtext('(portfolio = open red circle; benchmark = open blue triangle)',
                   side=3, line=0.8, cex=1)
             mtext('(Schwab EF = solid line; S&P 500 / AGG EF = dotted line)',
                   side=3, line=0, cex=1)
@@ -170,20 +174,21 @@ portfolio.plot <- function(twri=NA, twrc=NA, perf=NA, twri.ef=NA,
         if (requested == 'ab') {
             ## create plot
             xrange <- range(perf[1:(nhold+2),]$beta)
-            xlim   <- c(min(xrange),
-                        max(xrange) + 0.25*(max(xrange) - min(xrange)))
-            ylim   <- range(perf[1:(nhold+2),]$alpha)
+            xlim   <- c(min(xrange, na.rm = TRUE),
+                        max(xrange, na.rm = TRUE) + 
+                            0.25*(max(xrange, na.rm = TRUE) - min(xrange, na.rm = TRUE)))
+            ylim   <- range(perf[1:(nhold+2),]$alpha, na.rm = TRUE)
             with(perfhold, plotfit(beta, alpha, label, interval='noline',
-                                   xlimspec=xlim, ylimspec=ylim,
+                                   xlimspec=xlim, ylimspec=ylim, pch=pch.hold,
                                    xlabel = 'beta',
                                    ylabel = 'alpha',
                                    main   = main))
             ## add portfolio
-            points(perfport$beta, perfport$alpha, col='red', pch=16)
+            points(perfport$beta, perfport$alpha, col='red', pch=1)
             ## add benchmark
-            points(perfbench1$beta, perfbench1$alpha, col='blue', pch=17)
+            points(perfbench1$beta, perfbench1$alpha, col='blue', pch=2)
             ## add subtitle text
-            mtext('(portfolio = solid red circle; benchmark = solid blue triangle)',
+            mtext('(portfolio = open red circle; benchmark = open blue triangle)',
                   side=3, line=0.75, cex=1)        
         }
 
