88de972d9d367bb6bc1620c43ce61a1b5288c917
dhjelmar <dlhjel@gmail.com>
2023-06-27 22:15:14 -0400

    na.to.zero as as default added to function

--- modules/portfolio.summary.r
+++ modules/portfolio.summary.r
@@ -1,4 +1,4 @@
-portfolio.summary <- function(twri, value, twrib,
+portfolio.summary <- function(twri, value, twrib, na.to.zero=FALSE,
                               ##                   YTD       1 year       3 years      5 years
                               periods      = list('1 year', '12 months', '36 months', '60 months'),
                               period.names = list('TWRC YTD', 'TWRC 1-yr', 'TWRC 3-yr', 'TWRC 5-yr')) {
@@ -20,8 +20,9 @@ portfolio.summary <- function(twri, value, twrib,
     df <- data.frame(description = c(names(twri), 'portfolio', names(twrib)))
     
     ## convert any NA to 0
-    twri[is.na(twri)] <- 0 
-
+    if (isTRUE(na.to.zero)) twri[is.na(twri)] <- 0 
+    if (isTRUE(na.to.zero)) value[is.na(value)] <- 0 
+    
     ## check that twri and value have the same dates (needed for portfolio weighting over time)
     if (!identical(zoo::index(value), zoo::index(twri))) {
         ## problem with input value and twri not being consistent
@@ -36,7 +37,7 @@ portfolio.summary <- function(twri, value, twrib,
     value.port  <- apply(value , 1, sum)                    # sum rows
     value0.port <- apply(value0, 1, sum)                    # sum rows
     twri.port   <- value.port / value0.port - 1
-    weight      <- as.numeric( value[nrow(value),] / sum(value[nrow(value),]) )
+    weight      <- as.numeric( value[nrow(value),] / sum(value[nrow(value),]) )  # final weight
 
     ## add weights and values to df
     weight <- c(weight, 1, rep(NA, ncol(twrib)))
